5a9fdba9747229d4b40d2780755a407b6780898c
TERMUXPEMULA123 <79355260+TERMUXPEMULA123@users.noreply.github.com>
2021-02-21 02:10:22 +0700

    Add files via upload

---
+++ tolsanimasi.sh
@@ -0,0 +1,55 @@
+clear
+sleep 1
+echo
+toilet -f big -F gay By Mr.Redi
+echo $green"selamat mencoba"
+echo
+echo $red"01.)stabilkan jaringan"
+echo
+echo $red"02.)munculkan matrix"
+echo 
+echo $blue"03.)munculkan api"
+echo
+echo $blue"04.)kembali aja"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+toilet -f big -F Rd tutorial
+echo $green"stabilkan jaringan"
+sleep 1
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+toilet -f big -F gay Rd Tutorial
+echo $green"menampilkan matrix"
+sleep 1
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+toilet -f big -F gay By Mr Redi
+sleep
+pkg install libcaca
+cacafire
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+clear
+echo $yellow
+figlet "Bey"
+echo $yellow"To Be Continued"
+sleep 1
+Exit
+fi
+
