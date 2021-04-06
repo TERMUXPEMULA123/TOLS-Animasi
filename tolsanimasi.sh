6936f0a653f59954c8b56e8e779621b45ed35318
MR.PS1KOP4T <79355260+TERMUXPEMULA123@users.noreply.github.com>
2021-04-06 19:13:27 +0700

    tolsanimasi.sh

--- tolsanimasi.sh
+++ tolsanimasi.sh
@@ -1,7 +1,7 @@
 clear
 sleep 1
 echo
-toilet -f big -F gay By Mr.Redi
+toilet -f big -F gay By Diki Simarmata
 echo $green"selamat mencoba"
 echo
 echo $red"01.)stabilkan jaringan"
@@ -10,7 +10,9 @@ echo $red"02.)munculkan matrix"
 echo 
 echo $blue"03.)munculkan api"
 echo
-echo $blue"04.)kembali aja"
+echo $blue"04.)munculkan Kereta"
+echo
+echo $blue"05.)kembali aja"
 echo
 echo $white
 read -p "PilihMana : " bro
@@ -18,7 +20,7 @@ read -p "PilihMana : " bro
 if [ $bro = 01 ] || [ $bro = 01 ]
 then
 clear
-toilet -f big -F Rd tutorial
+toilet -f big -F Diki Simarmata
 echo $green"stabilkan jaringan"
 sleep 1
 ping -s1000 1.1.1.1
@@ -27,7 +29,7 @@ fi
 if [ $bro = 02 ] || [ $bro = 02 ]
 then
 clear
-toilet -f big -F gay Rd Tutorial
+toilet -f big -F gay Diki Simarmata
 echo $green"menampilkan matrix"
 sleep 1
 pkg install cmatrix
@@ -37,7 +39,7 @@ fi
 if [ $bro = 03 ] || [ $bro = 03 ]
 then
 clear
-toilet -f big -F gay By Mr Redi
+toilet -f big -F gay By Diki Simarmata
 sleep
 pkg install libcaca
 cacafire
@@ -46,10 +48,17 @@ fi
 if [ $bro = 04 ] || [ $bro = 04 ]
 then
 clear
+toilet -f big -F By Diki Simarmata
+sleep
+pkg install sl
+sl
+fi
+if [ $bro = 05 ] || [ $bro = 05 ]
+then
+clear
 echo $yellow
 figlet "Bey"
 echo $yellow"To Be Continued"
 sleep 1
 Exit
 fi
-
